8feec0d8afa4b2dcbc28a6450815046a39a0533f
leinaxd <leinad_reverso@yahoo.com.ar>
2020-11-22 20:15:33 -0300

    Hola mundo, este es un archivo de prueba para ver si tengo permiso en tu repositorio!

---
+++ TP3.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import Rotaciones as Rot
+import matplotlib.image as mpimg
+np.set_printoptions(formatter={'float': lambda x: "{0:0.2f}".format(x)})
+
+def jacobiano():
+    pass
+def problemaInversoNumerico(poseInicial,poseDeseada):
+    pass
+#############################################
+#                Ejercicios
+#############################################
+if __name__ == "__main__":
+    print "trabajo practico sobre velocidades y singularidades"
+    ej = 1
+    if ej == 1:
+        print "Obtencion del jacobiano del IRB140"
